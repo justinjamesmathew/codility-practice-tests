af431769467d8138d06684dec97b0129cf35911a
Justin James Mathew <45729311+justinjamesmathew@users.noreply.github.com>
2022-09-01 23:34:28 +0200

    Update maxProfit.js

--- maxProfit.js
+++ maxProfit.js
@@ -12,3 +12,29 @@ function solution(A) {
 }
 
 //inefficient solution does not work for large values O(n^2)
+
+//Below is the efficient solution O(n)
+
+function solution(A) {
+    // write your code in JavaScript (Node.js 8.9.4)
+    let i = 0;
+    let profit = 0;
+    let lowprice = A[0];
+    let tempprofit = 0;
+    let len = A.length;
+    for (i = 1; i < len; i++){
+        if(A[i] < lowprice) {
+           
+            lowprice = A[i];
+        }
+        else{
+            tempprofit = A[i]-lowprice;
+            if (tempprofit > profit)
+                profit = tempprofit;
+        }
+            
+        }
+    
+    return profit;
+};
+
